6068d0a040d4de133778a0659e423760022dce5c
Hossein <hossein.bajan@gmail.com>
2024-06-18 22:43:02 +0330

    Add some packages like prisma, pinia and pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,21 @@ export default defineNuxtConfig({
     },
   },
 
+  pages: true,
+
+  modules: [
+    "nuxt-icon",
+    // "@nuxtjs/supabase",
+    "@pinia/nuxt",
+    "@vite-pwa/nuxt",
+  ],
+
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URL,
+    },
+  },
+
   devtools: { enabled: true },
 
   alias: {},
@@ -18,9 +33,37 @@ export default defineNuxtConfig({
 
   plugins: ["~/plugins/bootstrap.client"],
 
+  pwa: {
+    manifest: {
+      name: "HBSOFT",
+      short_name: "HBSOFT",
+      description: "This is a HBSOFT project test",
+      theme_color: "#000000",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+  },
+
   app: {
     pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
     head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1, maximum-scale=1",
       htmlAttrs: {
         lang: "fa",
         dir: "rtl",
